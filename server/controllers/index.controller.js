8dc6eb4fa807dc69b143113ea1e6df5c47402c17
Piyush Rank <rankpiyush97@gmail.com>
2026-07-01 14:37:07 +0530

    feat: setup backend architecture

---
+++ server/controllers/index.controller.js
@@ -0,0 +1,9 @@
+const home = (req,res)=>{
+  res.send("PrepAI Backend Running 🚀");
+}
+
+const test = (req,res)=>{
+    res.send("this is  a testing ....");
+}
+
+export {home , test};
---
+++ server/server.js
@@ -0,0 +1,15 @@
+import app from "./app.js";
+
+
+const PORT = 3000;
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`app is listing port ${PORT}`);
+    
+})
+
